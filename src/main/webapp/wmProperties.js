6a0e84c114feb84a9c20df14bcbf7b3700c5f3b5
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-06 18:23:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_iPawLoIckL",
+  "homePage" : "Main",
+  "name" : "Auto_iPawLoIckL",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
